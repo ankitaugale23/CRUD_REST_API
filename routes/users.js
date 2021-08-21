3438ac118516b1cf7c76f1b9334d23f4ac4008b1
ankitaugale23 <ankitaugale23@gmail.com>
2021-08-21 18:55:21 +0530

    post user with id to database

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import {v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "Ankita",
-        lastName: "Ugale",
-        age: 20
-    },
-    {
-        firstName: "Tushar",
-        lastName: "Ugale",
-        age: 19
-    }
-]
+const users = []
 
 router.get('/',(req,res)=>    //functionality for '/users' route (default / means /users here).
 {
@@ -23,7 +13,8 @@ router.get('/',(req,res)=>    //functionality for '/users' route (default / mean
 
 router.post("/", (req,res) => {    //adds user to the users array
     const user = req.body;
-    users.push(user);
+
+    users.push({...user, id: uuidv4() });   //push user with id to the data
     res.send(`User with name ${user.firstName} successfully added`);   
 
 })
